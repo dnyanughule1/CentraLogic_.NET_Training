53c41d3675398abcec0992d9fe104575d4ffaab4
Dnyaneshwar Bandu Ghule <dnyanughule31@gmail.com>
2023-12-26 10:30:21 +0530

    validation

--- CRUD-Operations/Controllers/StudentController.cs
+++ CRUD-Operations/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 using System.ComponentModel;
+using System.Drawing.Text;
 using System.Reflection.Metadata.Ecma335;
 using Container = Microsoft.Azure.Cosmos.Container;
 
@@ -17,7 +18,7 @@ namespace CRUD_Operations.Controllers
         public string URI = "https://localhost:8081";
         public string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
         public string DatabaseName = "SampleDB";
-        public string ContainerName = "TestContainer";
+        public string ContainerName = "TestContainer1";
 
         public readonly Container _container; // null 
         public StudentController()
@@ -28,8 +29,17 @@ namespace CRUD_Operations.Controllers
         [HttpPost]
         public async Task<IActionResult> AddStudent(StudentModel studentModel)
         {
+            
             try
             {
+                //if roll number is aldready exist in database data not added
+                bool isRollNumberExists = await IsRollNumberExistsAsync(studentModel.RollNo);
+
+                if (isRollNumberExists)
+                {
+                    return BadRequest("Roll number already exists in the database.");
+                }
+                
                 Student studentEntity = new Student();
                 // Mapping 
                 studentEntity.Name = studentModel.Name;
@@ -55,7 +65,7 @@ namespace CRUD_Operations.Controllers
                 studentEntity.Archieved = false;  // Not Accesible to System
 
                 Student resposne = await _container.CreateItemAsync(studentEntity);
-
+                
                 // Reverse MApping 
                 studentModel.Name = resposne.Name;
                 studentModel.Age = resposne.Age;
@@ -68,6 +78,16 @@ namespace CRUD_Operations.Controllers
 
                 return BadRequest("Data Adding Failed" + ex);
             }
+
+            async Task<bool> IsRollNumberExistsAsync(int RollNo)
+            {
+                List<int> RollNumber = new List<int>();
+                {
+                    RollNumber.Add(RollNo);
+                }
+                return RollNumber.Contains(RollNo);
+            }
+
         }
 
         [HttpPost]
@@ -108,32 +128,49 @@ namespace CRUD_Operations.Controllers
                 return BadRequest("Data Get Failed");
             }
         }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteStudentByUId(string uId)
+        [HttpPut("{uId}")]
+        public async Task<IActionResult> UpdateStudentByUId(string uId, UpdatedStudentModel updatedStudentModel)
         {
             try
             {
-                var partitionKey = new PartitionKey("student");
+                // Retrieve the student by UId
+                Student existingStudent = _container.GetItemLinqQueryable<Student>(true).Where(q => q.DocumentType == "student" && q.UId == uId).AsEnumerable().FirstOrDefault();
 
-                // Use DeleteItemAsync to delete by Id and partition key
-                var deleteResponse = await _container.DeleteItemAsync<Student>(
-                    partitionKey: partitionKey,
-                    id: uId
-                );
-
-                if (deleteResponse.StatusCode == System.Net.HttpStatusCode.NoContent)
-                {
-                    return Ok($"Student with UId '{uId}' deleted successfully.");
-                }
-                else if (deleteResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+                if (existingStudent == null)
                 {
                     return NotFound($"Student with UId '{uId}' not found.");
                 }
-                else
-                {
-                    return StatusCode((int)deleteResponse.StatusCode, $"Failed to delete student with UId '{uId}'.");
-                }
+
+                // Increment the version by one
+                existingStudent.Version++;
+
+                // Update the properties of the existing student
+                existingStudent.Name = updatedStudentModel.Name;
+                existingStudent.Age = updatedStudentModel.Age;
+                existingStudent.RollNo = updatedStudentModel.RollNo;
+
+                // Use ReplaceItemAsync to update the student in CosmosDB
+                Student replaceResponse = await _container.ReplaceItemAsync<Student>(existingStudent, uId);
+
+                return Ok(replaceResponse);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Data Update Failed: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteStudentByUId(string uId, string partitionKey)
+        {
+            try
+            { 
+                // Use DeleteItemAsync to delete by Id and partition key
+                var deleteResponse = await _container.DeleteItemAsync<Student>(
+                   uId, new PartitionKey(partitionKey)
+                );
+                return Ok(deleteResponse);
+
             }
             catch (Exception ex)
             {
@@ -141,63 +178,6 @@ namespace CRUD_Operations.Controllers
             }
         }
 
-        [HttpPut("{uId}")]
-        public async Task<IActionResult> UpdateStudentByUId(string uId, UpdatedStudentModel updatedStudentModel)
-        {
-            try
-            {
-                // Retrieve the student by UId
-                Student existingStudent = _container.GetItemLinqQueryable<Student>(true)
-                    .Where(q => q.DocumentType == "student" && q.UId == uId)
-                    .AsEnumerable()
-                    .FirstOrDefault();
-
-                if (existingStudent == null)
-                {
-                    return NotFound($"Student with UId '{uId}' not found.");
-                }
-
-                // Update the properties of the existing student
-                existingStudent.Name = updatedStudentModel.Name;
-                existingStudent.Age = updatedStudentModel.Age;
-                existingStudent.RollNo = updatedStudentModel.RollNo;
-
-                existingStudent.UpdatedOn = DateTime.Now;
-                existingStudent.UpdatedByName = "Updated User";  // Replace with the actual user updating the record
-                existingStudent.UpdatedBy = "Updated User's UId";  // Replace with the actual user's UId updating the record
-
-                existingStudent.Version++; // Increment the version
-
-                // Use ReplaceItemAsync to update the student in CosmosDB
-                var replaceResponse = await _container.ReplaceItemAsync<Student>(
-                    partitionKey: new PartitionKey(existingStudent.DocumentType),
-                    id: existingStudent.Id,
-                    item: existingStudent
-                );
-
-                if (replaceResponse.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    // Reverse Mapping 
-                    var updatedStudentModel = new StudentModel
-                    {
-                        Name = existingStudent.Name,
-                        Age = existingStudent.Age,
-                        RollNo = existingStudent.RollNo
-                    };
-
-                    return Ok(updatedStudentModel);
-                }
-                else
-                {
-                    return StatusCode((int)replaceResponse.StatusCode, $"Failed to update student with UId '{uId}'.");
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Data Update Failed: {ex.Message}");
-            }
-        }
-   
 
         private Container GetContainer() // DRY
         {
--- CRUD-Operations/DTO/UpdatedStudentModel.cs
+++ CRUD-Operations/DTO/UpdatedStudentModel.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public string RollNo { get; set; }
+        public int RollNo { get; set; }
     }
+
 }
